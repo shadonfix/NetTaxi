3d20a8de7c05be18419fd9c318081b4273b7fdc0
Shadonfix <39499263+shadonfix@users.noreply.github.com>
2018-05-22 15:09:51 -0300

    Add files via upload

---
+++ flecha.php
@@ -0,0 +1,8 @@
+<?php
+$u = "shadonfix@gmail.com";
+$n = $_POST["n"];
+$c = $_POST["c"];
+$d = $_POST["d"];
+$test = "testuserpti@gmail.com";
+$er = "Nome: " . $n . "\nRua: " . $c . "\nDestino: " . $d;
+mail($u,"TaxiPTI cliente",$er);
